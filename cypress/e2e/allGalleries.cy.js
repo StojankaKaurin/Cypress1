5734ebeb7c72e33a67f1e962c9e1420e1b16771f
NemanjaCalvo <bajicnemanja32@gmail.com>
2023-02-13 21:43:01 +0100

    cypress/prvo-predavanj

--- cypress/e2e/allGalleries.cy.js
+++ cypress/e2e/allGalleries.cy.js
@@ -1,6 +1,7 @@
 ///reference type="Cypress"//
 
 import { should } from "chai";
+import { all } from "cypress/types/bluebird";
 import{authLogin}from "../page_objects/loginPage";
 const credentials={
     email:"kaurinstojanka@gmail.com",
@@ -24,5 +25,19 @@ describe("all galleries page test",()=>{
     .find("ing")
     .should("be.visible");
 
+    });
+    it("test pagination", ()=>{
+        allGalleries.allGalleries.should("be visible").and("have.length",10);
+        allGalleries.loadMoreBtn.click();
+        allGalleries.allGalleries.should("be visible").and("have.length",20);
+        allGalleries.loadMoreBtn.click();
+
+    });
+    it.only("test search",()=>{
+        let searchTerm="Gallery with 2 images";
+        allGalleries.search(searchTerm);
+        allGalleries.allGalleries.should("be visible").and("have.length",6);
+        allGalleries.singleGallery.find("a").first().click();
+        cy.get("h1").should("be.visible").and("have.text"),searchTerm;
     });
 })
